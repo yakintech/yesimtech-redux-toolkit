2b50ae50de09bb4c4d0f3a55afa37c85a44f7e00
cagatayyildiz <yildiz.cagatay@hotmail.com>
2024-06-01 10:05:45 +0300

    redux sample

---
+++ src/store/cartSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        value: []
+    },
+    reducers: {
+        addToCart: (state, action) => {
+            //sepette ürün varsa o ürünün miktarını bir arttırır. yoksa yeni ürün oluşturup ekler
+            var cartItem = state.value.find(q => q.id == action.payload.id)
+
+            if (cartItem) {
+                cartItem.quantity++
+            }
+            else {
+                let newCartItem = {
+                    id: action.payload.id,
+                    name: action.payload.name,
+                    unitPrice: action.payload.unitPrice,
+                    quantity: 1
+                }
+                state.value.push(newCartItem)
+            }
+        },
+        removeFromCart: (state, action) => {
+            state.value = state.value.filter(q => q.id != action.payload)
+        }
+    }
+})
+
+export default cartSlice.reducer
+
+export const { addToCart, removeFromCart } = cartSlice.actions
